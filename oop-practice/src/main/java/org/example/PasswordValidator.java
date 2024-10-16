da8bdd01fa084093bc7c8cf5e9834a11326138a5
jeongho <leejh9128@gmail.com>
2024-10-16 13:00:34 +0900

    FC part1 - 테스트코드 : 테스트코드 실습

--- oop-practice/src/main/java/org/example/PasswordValidator.java
+++ oop-practice/src/main/java/org/example/PasswordValidator.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class PasswordValidator {
 
+    public static final String WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE = "비밀번호는 최소 8자 이상 12자 이하여야 한다.";
+
     /**
      * 비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.
      */
@@ -9,7 +11,9 @@ public class PasswordValidator {
         int length = password.length();
 
         if (length < 8 || length > 12) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+            throw new IllegalArgumentException(WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE);
         }
     }
+
+
 }
--- oop-practice/src/test/java/org/example/PasswordValidatorTest.java
+++ oop-practice/src/test/java/org/example/PasswordValidatorTest.java
@@ -14,6 +14,7 @@ public class PasswordValidatorTest {
      * 비밀번호는 최소 8자 이상 12자 이하여야 한다.
      * 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다.
      * 경계조건에 대해 테스트 코드를 작성해야 한다.
+     * 위와 같은 경우에는 7, 13자가 경계조건에 해당한다.
      */
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
