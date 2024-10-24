600dc7c59615f4a11522b0d3373a5992cee23d48
jeongho96 <leejh9128@gmail.com>
2024-10-24 14:43:52 +0900

    FC Part1 - JDBC 프로그래밍
    - h2 DB를 활용해 connectionPool 이해하기

---
+++ jdbc-practice/src/test/resources/db_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE USERS(
+    userId  varchar(12) not null ,
+    password varchar(12)    not null ,
+    name    varchar(20) not null ,
+    email   varchar(50),
+    primary key(userId)
+);
